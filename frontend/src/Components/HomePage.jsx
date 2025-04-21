fa6586f17186cb7eb2c848bf2ef393d66184be5c
unknown <iamna.jamal@gmail.com>
2025-04-21 13:27:26 +0500

    Merging Amna's code in Main

--- frontend/src/Components/HomePage.jsx
+++ frontend/src/Components/HomePage.jsx
@@ -4,7 +4,7 @@ function HomePage(){
     return (
         <main>
             <div>
-                <h1>Welcome to Team<i>UP</i></h1>
+                <h1 className='text-xl font-bold text-blue-600'>Welcome to Team<i className='text-blue-600/50 dark:text-sky-400/50'>UP</i></h1>
             </div>
         </main>
     );
